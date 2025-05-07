827fcbf816fe4eb6c546c94ddd858fb0432c6720
Ruyi Wu <147005026+Jswuzh@users.noreply.github.com>
2025-05-07 01:45:24 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -44,7 +44,7 @@ function init() {
         if (volume === 0) {
             volumeIcon.src = './assets/icons/volume-level-0.svg';
         } else if (volume < 33) {
-            volumeIcon.src = '.assets/icons/volume-level-1.svg';
+            volumeIcon.src = './assets/icons/volume-level-1.svg';
         } else if (volume < 67) {
             volumeIcon.src = './assets/icons/volume-level-2.svg';
         } else {
