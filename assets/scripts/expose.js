9eea67a0deff250442b6e58c3fc667d95157ea0b
Ruyi Wu <147005026+Jswuzh@users.noreply.github.com>
2025-05-07 01:25:12 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -19,19 +19,19 @@ function init() {
         // Update image and audio source
         switch(selectedHorn) {
             case 'air-horn':
-                imageDisplay.src = 'assets/images/air-horn.svg';
-                audioElement.src = 'assets/audio/air-horn.mp3';
+                imageDisplay.src = './assets/images/air-horn.svg';
+                audioElement.src = './assets/audio/air-horn.mp3';
                 break;
             case 'car-horn':
-                imageDisplay.src = 'assets/images/car-horn.svg';
-                audioElement.src = 'assets/audio/car-horn.mp3';
+                imageDisplay.src = './assets/images/car-horn.svg';
+                audioElement.src = './assets/audio/car-horn.mp3';
                 break;
             case 'party-horn':
-                imageDisplay.src = 'assets/images/party-horn.svg';
-                audioElement.src = 'assets/audio/party-horn.mp3';
+                imageDisplay.src = './assets/images/party-horn.svg';
+                audioElement.src = './assets/audio/party-horn.mp3';
                 break;
             default:
-                imageDisplay.src = 'assets/images/no-image.png';
+                imageDisplay.src = './assets/images/no-image.png';
                 audioElement.src = '';
         }
         
@@ -42,13 +42,13 @@ function init() {
         
         // Update volume icon in real-time
         if (volume === 0) {
-            volumeIcon.src = 'assets/icons/volume-level-0.svg';
+            volumeIcon.src = './assets/icons/volume-level-0.svg';
         } else if (volume < 33) {
-            volumeIcon.src = 'assets/icons/volume-level-1.svg';
+            volumeIcon.src = '.assets/icons/volume-level-1.svg';
         } else if (volume < 67) {
-            volumeIcon.src = 'assets/icons/volume-level-2.svg';
+            volumeIcon.src = './assets/icons/volume-level-2.svg';
         } else {
-            volumeIcon.src = 'assets/icons/volume-level-3.svg';
+            volumeIcon.src = './assets/icons/volume-level-3.svg';
         }
         
         // ACTUALLY set the audio volume (converted from 0-100 to 0-1)
